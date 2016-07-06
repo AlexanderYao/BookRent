ff528126a8aa3bacc7c75ad6d33d221acd0042e1
AlexanderYao <dwy4670@gmail.com>
2016-07-06 17:58:29 +0800

    尝试转为xml可配置化

---
+++ BookRent/Config/PluginNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BookRent
+{
+    public class PluginNode
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("isOn")]
+        public bool IsOn { get; set; }
+    }
+}
