a0abb982b24555f2f13a1c5127f8704614f58096
AlexanderYao <dwy4670@gmail.com>
2017-02-05 16:59:29 +0800

    添加单个会员编辑页面

--- BookRent.Web/Controller/PersonCtrl.js
+++ BookRent.Web/Controller/PersonCtrl.js
@@ -1,4 +1,5 @@
 var BaseCtrl = require('./../base/BaseCtrl');
+var enums = require('./../util/enums');
 
 function PersonCtrl(containerId, params){
     BaseCtrl.call(this, containerId, params);
@@ -10,11 +11,46 @@ function PersonCtrl(containerId, params){
         this.vm = new Vue({
             el: '#'+ctrl.containerId,
             data:{
-
+                action:'',
+                person:{},
+                Sexes:enums.Sexes
             },
             methods:{
                 query:function(){
+                    if(ctrl.params.id == 'add'){
+                        this.action = '新增';
+                        this.$set(this, 'person', {});
+                        return;
+                    }
 
+                    this.action = '编辑';
+                    var that = this;
+                    var promise = $.get('person/'+ctrl.params.id);
+                    promise.done(function(data){
+                        that.$set(that, 'person', JSON.parse(data));
+                    });
+                },
+                save:function(){
+                    console.log('save person:');
+                    console.log(this.person);
+                    var promise = $.ajax({
+                        type:'POST',
+                        url:'person/save',
+                        data:JSON.stringify(this.person),
+                        contentType:'application/json'
+                    });
+                    promise.done(function(res){
+                        console.log('save person: id = '+res);
+                        UIkit.notify('保存成功!',{status:'info'});
+                        ctrl.close();
+                    });
+                    promise.fail(function(error){
+                        console.log(error);
+                    });
+                },
+                cancel:function(){
+                    window.history.go(-1);
+                    //ctrl.close();
                 }
             }
         });
--- BookRent.Web/base/bundle.js
+++ BookRent.Web/base/bundle.js
@@ -140,6 +140,7 @@ module.exports = MainCtrl;
 
 },{"./../base/BaseCtrl":8}],4:[function(require,module,exports){
 var BaseCtrl = require('./../base/BaseCtrl');
+var enums = require('./../util/enums');
 
 function PersonCtrl(containerId, params){
     BaseCtrl.call(this, containerId, params);
@@ -151,11 +152,46 @@ function PersonCtrl(containerId, params){
         this.vm = new Vue({
             el: '#'+ctrl.containerId,
             data:{
-
+                action:'',
+                person:{},
+                Sexes:enums.Sexes
             },
             methods:{
                 query:function(){
+                    if(ctrl.params.id == 'add'){
+                        this.action = '新增';
+                        this.$set(this, 'person', {});
+                        return;
+                    }
 
+                    this.action = '编辑';
+                    var that = this;
+                    var promise = $.get('person/'+ctrl.params.id);
+                    promise.done(function(data){
+                        that.$set(that, 'person', JSON.parse(data));
+                    });
+                },
+                save:function(){
+                    console.log('save person:');
+                    console.log(this.person);
+                    var promise = $.ajax({
+                        type:'POST',
+                        url:'person/save',
+                        data:JSON.stringify(this.person),
+                        contentType:'application/json'
+                    });
+                    promise.done(function(res){
+                        console.log('save person: id = '+res);
+                        UIkit.notify('保存成功!',{status:'info'});
+                        ctrl.close();
+                    });
+                    promise.fail(function(error){
+                        console.log(error);
+                    });
+                },
+                cancel:function(){
+                    window.history.go(-1);
+                    //ctrl.close();
                 }
             }
         });
@@ -164,7 +200,7 @@ function PersonCtrl(containerId, params){
 }
 module.exports = PersonCtrl;
 
-},{"./../base/BaseCtrl":8}],5:[function(require,module,exports){
+},{"./../base/BaseCtrl":8,"./../util/enums":10}],5:[function(require,module,exports){
 var BaseCtrl = require('./../base/BaseCtrl');
 
 function PersonListCtrl(containerId, params){
