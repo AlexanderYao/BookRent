9bceaa0a7ebb12e5116f9fc3852fe7c0d4883654
AlexanderYao <dwy4670@gmail.com>
2017-08-17 09:25:22 +0800

    a little refactor

--- BookRentApp/app/pages/topup.js
+++ BookRentApp/app/pages/topup.js
@@ -6,7 +6,7 @@ import {
 	TouchableOpacity,
 } from 'react-native';
 import Button from 'apsl-react-native-button';
-import Toast from 'react-native-root-toast';
+import Toast from '../utils/toast';
 import RadioForm, {
 	RadioButton,
 	RadioButtonInput,
@@ -155,86 +155,93 @@ export default class TopupScreen extends React.Component {
 		}}>{optionNodes}</View>
 	}
 
-	async topupNow(){
+	topupNow(){
 		switch(this.state.payType){
 			case 'zhifubao':
-				let params = {
-					orderString: 'app_id=2015052600090779&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.01%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%22IQJZSRC1YMQB5HU%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2Fdomain.merchant.com%2Fpayment_notify&sign_type=RSA2&timestamp=2016-08-25%2020%3A26%3A31&version=1.0&sign=cYmuUnKi5QdBsoZEAbMXVMmRWjsuUj%2By48A2DvWAVVBuYkiBj13CFDHu2vZQvmOfkjE0YqCUQE04kqm9Xg3tIX8tPeIGIFtsIyp%2FM45w1ZsDOiduBbduGfRo1XRsvAyVAv2hCrBLLrDI5Vi7uZZ77Lo5J0PpUUWwyQGt0M4cj8g%3D',
-				};
-				console.log(Alipay);
-				Alipay.pay(params.orderString).then((data) => {
-					if (data.length && data[0].resultStatus) {
-						/*处理支付结果*/
-						switch (data[0].resultStatus) {
-							case "9000":
-								opt.success && opt.success(data)
-								break;
-							case "8000":
-								opt.fail && opt.fail('支付结果未知,请查询订单状态')
-								break;
-							case "4000":
-								opt.fail && opt.fail('订单支付失败')
-								break;
-							case "5000":
-								opt.fail && opt.fail('重复请求')
-								break;
-							case "6001":
-								opt.fail && opt.fail('用户中途取消')
-								break;
-							case "6002":
-								opt.fail && opt.fail('网络连接出错')
-								break;
-							case "6004":
-								opt.fail && opt.fail('支付结果未知,请查询订单状态')
-								break;
-							default:
-								opt.fail && opt.fail('其他失败原因')
-								break;
-						}
-					} else {
-						opt.fail && opt.fail('其他失败原因')
-					} 
-				}, (err) => {
-					opt.fail && opt.fail('支付失败，请重新支付')
-				});
+				this.alipay();
 				break;
 			case 'weixin':
-				console.log('weixin pay');
-				let res = {
-					appid: 'wx8888888888888888',	/*微信开放平台审核通过的应用APPID*/
-		      partnerId: '1900000109',  		/*商家向财付通申请的商家id*/
-		      prepayId: 'WX1217752501201407033233368018',    /*预支付订单*/
-		      nonceStr: '5K8264ILTKCH16CQ2502SI8ZNMTM67VS',    /*随机串，防重发*/
-		      timeStamp: '1412000000',  /*时间戳，防重发*/
-		      package: 'Sign=WXPay',      /*商家根据财付通文档填写的数据和签名*/
-		      sign: 'C380BEC2BFD727A4B6845133519F3AD6',            /*商家根据微信开放平台文档对数据做的签名*/
-				};
-
-				try {
-			    let result = await WeChat.pay({
-			      partnerId: res.partnerid,  /*商家向财付通申请的商家id*/
-			      prepayId: res.prepayid,    /*预支付订单*/
-			      nonceStr: res.noncestr,    /*随机串，防重发*/
-			      timeStamp: res.timestamp,  /*时间戳，防重发*/
-			      package: res.package,      /*商家根据财付通文档填写的数据和签名*/
-			      sign: res.sign,            /*商家根据微信开放平台文档对数据做的签名*/
-			    });
-
-			    console.log(result);
-			    if(result.errCode === 0){
-			    	Toast.show('支付成功', {duration:1000});
-			    }else if(result.errCode === -1){
-			    	Toast.show('订单支付失败：'+result.errStr, {duration:1000});
-			    }else if(result.errCode === -2){
-			    	Toast.show('用户取消', {duration:1000});
-			    }
-			  } catch (error) {
-			  	console.log(error);
-			  	Toast.show('失败：'+error, {duration:1000});
-			  }
+				this.wxpay();
 				break;
 			default:
 				console.error('unknown payType');
 		}
+	}
+
+	alipay(){
+		let params = {
+			orderString: 'app_id=2015052600090779&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.01%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%22IQJZSRC1YMQB5HU%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2Fdomain.merchant.com%2Fpayment_notify&sign_type=RSA2&timestamp=2016-08-25%2020%3A26%3A31&version=1.0&sign=cYmuUnKi5QdBsoZEAbMXVMmRWjsuUj%2By48A2DvWAVVBuYkiBj13CFDHu2vZQvmOfkjE0YqCUQE04kqm9Xg3tIX8tPeIGIFtsIyp%2FM45w1ZsDOiduBbduGfRo1XRsvAyVAv2hCrBLLrDI5Vi7uZZ77Lo5J0PpUUWwyQGt0M4cj8g%3D',
+		};
+		console.log(Alipay);
+		Alipay.pay(params.orderString).then((data) => {
+			if (data.length && data[0].resultStatus) {
+				switch (data[0].resultStatus) {
+					case "9000":
+						Toast.show('支付成功')
+						break;
+					case "8000":
+						Toast.show('支付结果未知,请查询订单状态')
+						break;
+					case "4000":
+						Toast.show('订单支付失败')
+						break;
+					case "5000":
+						Toast.show('重复请求')
+						break;
+					case "6001":
+						Toast.show('用户中途取消')
+						break;
+					case "6002":
+						Toast.show('网络连接出错')
+						break;
+					case "6004":
+						Toast.show('支付结果未知,请查询订单状态')
+						break;
+					default:
+						Toast.show('其他失败原因')
+						break;
+				}
+			} else {
+				Toast.show('其他失败原因')
+			} 
+		}, (err) => {
+			Toast.show('支付失败，请重新支付')
+		});
+	}
+
+	async wxpay(){
+		console.log('weixin pay');
+		let res = {
+			appid: 'wx8888888888888888',	/*微信开放平台审核通过的应用APPID*/
+      partnerId: '1900000109',  		/*商家向财付通申请的商家id*/
+      prepayId: 'WX1217752501201407033233368018',    /*预支付订单*/
+      nonceStr: '5K8264ILTKCH16CQ2502SI8ZNMTM67VS',    /*随机串，防重发*/
+      timeStamp: '1412000000',  /*时间戳，防重发*/
+      package: 'Sign=WXPay',      /*商家根据财付通文档填写的数据和签名*/
+      sign: 'C380BEC2BFD727A4B6845133519F3AD6',            /*商家根据微信开放平台文档对数据做的签名*/
+		};
+
+		try {
+	    let result = await WeChat.pay({
+	      partnerId: res.partnerid,  /*商家向财付通申请的商家id*/
+	      prepayId: res.prepayid,    /*预支付订单*/
+	      nonceStr: res.noncestr,    /*随机串，防重发*/
+	      timeStamp: res.timestamp,  /*时间戳，防重发*/
+	      package: res.package,      /*商家根据财付通文档填写的数据和签名*/
+	      sign: res.sign,            /*商家根据微信开放平台文档对数据做的签名*/
+	    });
+
+	    console.log(result);
+	    if(result.errCode === 0){
+	    	Toast.show('支付成功');
+	    }else if(result.errCode === -1){
+	    	Toast.show('订单支付失败：'+result.errStr);
+	    }else if(result.errCode === -2){
+	    	Toast.show('用户取消');
+	    }
+	  } catch (error) {
+	  	console.log(error);
+	  	Toast.show('失败：'+error);
+	  }
 	}
 }
